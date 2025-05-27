2e41b1ba2b806e28443e0680c0f456b9d5fb7298
SooyounJo <dus10167@gmail.com>
2025-05-28 06:14:43 +0900

    회전

--- pages/test.js
+++ pages/test.js
@@ -55,14 +55,39 @@ function GlassModel() {
 
 function LPModelTest() {
   const { scene } = useGLTF('/3d/recode/lp5.glb');
+  const groupRef = React.useRef();
+  const [center, setCenter] = React.useState([0, 0, 0]);
+
+  React.useEffect(() => {
+    if (scene) {
+      // 메시 전체의 중심 계산
+      const box = new THREE.Box3().setFromObject(scene);
+      const centerVec = new THREE.Vector3();
+      box.getCenter(centerVec);
+      // 피봇을 메시 중심으로 이동
+      scene.position.sub(centerVec);
+      setCenter([centerVec.x, centerVec.y, centerVec.z]);
+      // 메시별 머티리얼 처리
+      scene.traverse((child) => {
+        if (child.isMesh) {
+          if (!child.material.map) {
+            child.material = new THREE.MeshStandardMaterial({ color: 0x000000 });
+          }
+        }
+      });
+    }
+  }, [scene]);
+
+  useFrame(() => {
+    if (groupRef.current) {
+      groupRef.current.rotation.y += 0.004;
+    }
+  });
+
   return (
-    <primitive 
-      object={scene} 
-      position={[-5.5, -3.7, 8.2]}
-      scale={8}
-      receiveShadow
-      castShadow
-    />
+    <group ref={groupRef} position={[-5.5, -3.7, 8.2]} scale={8} receiveShadow castShadow>
+      <primitive object={scene} />
+    </group>
   );
 }
 
